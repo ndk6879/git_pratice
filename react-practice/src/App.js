ebfa02b47b4b9a84cbbec04efbeb870ba5aea7dc
Donggyun Nam <ndk6879@naver.com>
2023-01-23 21:26:39 +0900

    잘 되구나 리태식이git status

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          리액트 푸쉬됬다 으하하
         </p>
         <a
           className="App-link"
